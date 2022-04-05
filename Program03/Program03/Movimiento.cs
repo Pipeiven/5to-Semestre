cd5f4d1eb982485fb29ca2a524a809f3ff474b62
Pipeiven <60204470+Pipeiven@users.noreply.github.com>
2022-04-04 22:11:11 -0400

    Clase 3, Tarjeta de Crédito

---
+++ Program03/Program03/Movimiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program03
+{
+    class Movimiento
+    {
+        public string Tipo { get; set; }
+
+        public int Monto { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int NumeroTransaccion { get; set; }
+    }
+}
---
+++ Program03/Program03/Tarjeta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program03
+{
+    class Tarjeta
+    {
+        public int NumeroTarjeta { get; set; }
+
+
+        private int Saldo { get; set; }
+
+        public string Vencimiento { get; set; }
+
+        public string CodigoTarjeta { get; set; }
+
+        public int Cupo { get; set; }
+
+        public string Disponibilidad { get; set; }
+
+        // Métodos
+
+        public string EstadoCuenta { get; set; }
+
+        public string Compra { get; set; }
+    }
+}
